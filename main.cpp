b927ad191660578cb7d77ad6665bc949b1280849
machukhinktato <tarabrinmv@gmail.com>
2021-08-04 09:44:52 +0300

    patterns is done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include <QPushButton>
 #include "lines.h"
 #include "colours.h"
+#include "patterns.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
 //    Lines window;
-    Colours window;
+//    Colours window;
+    Patterns window;
+
 
     window.resize(360, 280);
     window.setWindowTitle("MACHUKHINKTATO AREA");
